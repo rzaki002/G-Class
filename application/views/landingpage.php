4de3eb26ed113c920981a99588ab077f303363bf
rzaki002 <zakirahmat730@gmail.com>
2023-04-29 18:18:12 +0700

    upload source code landing page with ci3 bootstrap 5

---
+++ application/views/landingpage.php
@@ -0,0 +1,323 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <title>G-Class</title>
+    <link rel="shortcut icon" href="asset/img/hero-img.svg" type="image/x-icon">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="<?= base_url('assets/js/scrollreveal.js'); ?>"></script>
+</head>
+
+<body>
+    <!-- header -->
+    <nav class="navbar navbar-expand-lg ">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="<?= base_url('assets/img/brand/logo-gclass.svg'); ?>" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav py-2">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">Product</a>
+                </div>
+                <a class="btn btn-outline-secondary shadow-sm d-sm d-block" href="#">Get Class Now</a>
+            </div>
+        </div>
+    </nav>
+    <!-- end header -->
+
+    <!-- hero section -->
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text">
+                        Making websites easier and faster with G-Class
+                    </div>
+                    <div class="buttons">
+                        <a href="" class="btn btn-primary">Login</a>
+                        <a href="" class="btn btn-outline-secondary ms-3 ">Register</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="imghero">
+                        <img src="<?= base_url('assets/img/Saly-1.svg'); ?>" alt="hero-img" class="w-100">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end hero section -->
+
+    <!-- setup -->
+    <section class="setup">
+        <div class="container">
+            <div class="text-header text-center">
+                <h3>Get Started quickly without the hassle</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, possimus? Est, quis temporibus
+                    eius id maiores sit provident, ad, incidunt nam quaerat dolorum? Consequuntur voluptas architecto,
+                    ipsa suscipit maxime sed?</p>
+                <div class="imgstarted">
+                    <img src="<?= base_url('assets/img/Saly-10.svg'); ?>" alt="w-100">
+                </div>
+            </div>
+            <div class="items text-center">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="icons">
+                            <img src="<?= base_url('assets/img/icons/icons1.svg'); ?>" alt="">
+                        </div>
+                        <div class="desc">
+                            <h5>Easy</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, porro quasi. Quam et
+                                repellat cupiditate deleniti, a accusantium in quo nemo odit veniam, pariatur eaque
+                                inventore corporis sequi? Quibusdam, ipsum!</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="icons">
+                            <img src="<?= base_url('assets/img/icons/icons2.svg'); ?>" alt="">
+                        </div>
+                        <div class="desc">
+                            <h5>Friendly Mentor</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, porro quasi. Quam et
+                                repellat cupiditate deleniti, a accusantium in quo nemo odit veniam, pariatur eaque
+                                inventore corporis sequi? Quibusdam, ipsum!</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="icons">
+                            <img src="<?= base_url('assets/img/icons/icons3.svg'); ?>" alt="">
+                        </div>
+                        <div class="desc">
+                            <h5>Best Practice</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, porro quasi. Quam et
+                                repellat cupiditate deleniti, a accusantium in quo nemo odit veniam, pariatur eaque
+                                inventore corporis sequi? Quibusdam, ipsum!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end setup -->
+
+    <!-- information -->
+    <section class="information">
+        <div class="container">
+            <div class="row info-1">
+                <div class="col-md-6">
+                    <div class="text-information">
+                        <h5>The best solution learn to make websites faster and more precise</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi cum libero cupiditate
+                            repudiandae debitis? Molestiae veritatis rerum at, reprehenderit dolores ut nostrum
+                            similique earum sint doloribus nulla itaque voluptatibus laboriosam?</p>
+                    </div>
+                </div>
+                <div class="gbrgrafik col-md-6">
+                    <img src="<?= base_url('assets/img/image-1.svg"'); ?>" alt="image-1" class="w-100">
+                </div>
+            </div>
+            <div class="row info-2">
+
+                <div class="gbrgrafik col-md-6">
+                    <img src="<?= base_url('assets/img/image-2.svg'); ?>" alt="image-2" class="w-100">
+                </div>
+                <div class="col-md-6">
+                    <div class="text-information">
+                        <h5>The best solution learn to make websites faster and more precise</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi cum libero cupiditate
+                            repudiandae debitis? Molestiae veritatis rerum at, reprehenderit dolores ut nostrum
+                            similique earum sint doloribus nulla itaque voluptatibus laboriosam?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end information -->
+    <!-- footer -->
+    <footer class="footer">
+
+        <!-- Widgets - Bootstrap Brain Component -->
+        <section class="bg-light border-top py-4 py-md-5 py-xl-8">
+            <div class="container overflow-hidden">
+                <div class="row gy-4 gy-lg-0 justify-content-xl-between">
+                    <div class="col-12 col-md-4 col-lg-3 col-xl-2">
+                        <div class="widget">
+                            <a href="#!">
+                                <img src="<?= base_url('assets/img/brand/logo-gclass.svg'); ?>" alt="" width="175" height="57">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-3 col-xl-2">
+                        <div class="widget">
+                            <h4 class="widget-title mb-4">Get in Touch</h4>
+                            <address class="mb-4">Jalan Pangeran Ayin No. 57</address>
+                            <p class="mb-1">
+                                <a class="link-secondary text-decoration-none" href="tel:+15057922430">0895615426427</a>
+                            </p>
+                            <p class="mb-0">
+                                <a class="link-secondary text-decoration-none" href="#">gclas@gmail.com</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-3 col-xl-2">
+                        <div class="widget">
+                            <h4 class="widget-title mb-4">Learn More</h4>
+                            <ul class="list-unstyled">
+                                <li class="mb-2">
+                                    <a href="#!" class="link-secondary text-decoration-none">About</a>
+                                </li>
+                                <li class="mb-2">
+                                    <a href="#!" class="link-secondary text-decoration-none">Contact</a>
+                                </li>
+                                <li class="mb-2">
+                                    <a href="#!" class="link-secondary text-decoration-none">Advertise</a>
+                                </li>
+                                <li class="mb-2">
+                                    <a href="#!" class="link-secondary text-decoration-none">Terms of Service</a>
+                                </li>
+                                <li class="mb-0">
+                                    <a href="#!" class="link-secondary text-decoration-none">Privacy Policy</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-3 col-xl-4">
+                        <div class="widget">
+                            <h4 class="widget-title mb-4">Our Newsletter</h4>
+                            <p class="mb-4">Subscribe to our newsletter to get our news & discounts delivered to you.
+                            </p>
+                            <form action="#!">
+                                <div class="row gy-4">
+                                    <div class="col-12">
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="email-addon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                                                </svg>
+                                            </span>
+                                            <input type="email" class="form-control" id="email" value="" placeholder="Email Address" aria-label="email" aria-describedby="email-addon" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-grid">
+                                            <button class="btn btn-primary" type="submit">Subscribe</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Copyright - Bootstrap Brain Component -->
+        <section class="bg-light border-top py-4 py-md-5 py-xl-8">
+            <div class="container overflow-hidden">
+                <div class="row gy-4 gy-md-0">
+                    <div class="col-xs-12 col-md-7 order-1 order-md-0">
+                        <div class="copyright text-center text-md-start">
+                            &copy; 2023. All Rights Reserved.
+                        </div>
+                        <div class="credits text-secondary text-center text-md-start mt-2 fs-7">
+                            Built by <a href="https://bootstrapbrain.com/" class="link-secondary text-decoration-none">G-Class</a> with Rahmat Zaki <span class="text-primary">&#9829;</span>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-md-5 order-0 order-md-1">
+                        <ul class="nav justify-content-center justify-content-md-end">
+                            <li class="nav-item">
+                                <a class="nav-link link-dark px-2" href="#!">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                                    </svg>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link link-dark px-2" href="#!">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+                                        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
+                                    </svg>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link link-dark px-2" href="#!">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                        <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                                    </svg>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link link-dark px-2" href="#!">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                        <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+                                    </svg>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </footer>
+    <!-- end footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+    <script>
+        ScrollReveal().reveal('.imghero', {
+            origin: 'right',
+            distance: '100px',
+            scale: 0.85,
+            delay: 500,
+            reset: true
+        });
+        ScrollReveal().reveal('.imgstarted', {
+            origin: 'top',
+            distance: '100px',
+            scale: 0.85,
+            delay: 500,
+            reset: true
+        });
+        ScrollReveal().reveal('.widget', {
+            origin: 'top',
+            distance: '100px',
+            scale: 0.85,
+            delay: 500,
+            reset: true
+        });
+        ScrollReveal().reveal('.gbrgrafik', {
+            origin: 'top',
+            distance: '100px',
+            scale: 0.85,
+            delay: 500,
+            reset: true
+        });
+        ScrollReveal().reveal('.desc', {
+            origin: 'buttom',
+            distance: '100px',
+            scale: 0.85,
+            delay: 500,
+            reset: true
+        });
+        ScrollReveal().reveal('.icons', {
+            origin: 'top',
+            distance: '100px',
+            scale: 0.85,
+            delay: 500,
+            reset: true
+        });
+    </script>
+</body>
+
+</html>
